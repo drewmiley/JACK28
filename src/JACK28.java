410c527dc74b86a587a22b4d3c64f2896380ad53
Drew Miley <drewmiley92@gmail.com>
2018-07-15 20:56:37 +0100

    Initialises game state

--- src/JACK28.java
+++ src/JACK28.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JACK28 {
 
     public static void main(String[] args) {
+        int NUMBER_OF_PLAYERS = 2;
+        int INITIAL_HAND_SIZE = 7;
         Deck deck = new Deck();
+        Rules rules = new Rules();
+        Card topCard = deck.draw();
+        Pile pile = new Pile(topCard, rules);
+        List<Player> players = Arrays.stream(new Integer[NUMBER_OF_PLAYERS])
+                .map(a -> {
+                    List<Card> hand = Arrays.stream(new Integer[INITIAL_HAND_SIZE])
+                            .map(b -> deck.draw())
+                            .collect(Collectors.toList());
+                    return new Player(hand, rules, topCard);
+                })
+                .collect(Collectors.toList());
+        playGame(deck, pile, players);
+    }
+
+    private static void playGame(Deck deck, Pile pile, List<Player> players) {
         System.out.println("Hello, World");
     }
 
--- src/Player.java
+++ src/Player.java
@@ -5,10 +5,16 @@ class Player {
 
     private List<Card> hand;
     private Rules rules;
+    private Card topCard;
 
-    Player(List<Card> hand, Rules rules) {
+    void setTopCard(Card topCard) {
+        this.topCard = topCard;
+    }
+
+    Player(List<Card> hand, Rules rules, Card topCard) {
         this.hand = hand;
         this.rules = rules;
+        this.topCard = topCard;
     }
 
     List<Card> cardsToPlay() {
