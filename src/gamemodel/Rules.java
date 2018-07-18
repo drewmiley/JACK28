609f45b23dfdb92c627c36884ae3c9918a54bd49
Drew Miley <drewmiley92@gmail.com>
2018-07-18 23:19:53 +0100

    Adds rules constants

--- src/gamemodel/Rules.java
+++ src/gamemodel/Rules.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 public class Rules {
 
-    public Rules() {
+    public final FaceValue NOMINATE_SUIT = FaceValue.JACK;
+    public final boolean NOMINATE_SUIT_MUST_FOLLOW = false;
 
-    }
+    public final FaceValue DRAW_CARD = FaceValue.TWO;
+    public final int DRAW_CARD_NUMBER = 2;
+
+    public final FaceValue MISS_A_GO = FaceValue.EIGHT;
+
+    public final FaceValue SWITCH_DIRECTION = FaceValue.SEVEN;
+    public final boolean CAN_SWITCH_DIRECTION = false;
+
+    public final boolean RUN_UP_IN_SUIT = false;
+    public final boolean RUN_DOWN_IN_SUIT = false;
+    public final boolean RUN_FACEVALUE = true;
 
     public boolean isAllowedPlay(Card topCard, List<Card> cardsToPlay) {
         return cardsToPlay.size() == 1 &&
