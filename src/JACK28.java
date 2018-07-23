da3709900b3d6f7f2531182e69ff96e41b3f3766
Drew Miley <drew.miley@opencastsoftware.com>
2018-07-23 12:39:50 +0100

    Prints out winners

--- src/JACK28.java
+++ src/JACK28.java
@@ -8,6 +8,7 @@ import players.PlayerTypes;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -23,12 +24,15 @@ public class JACK28 {
         };
         int INITIAL_HAND_SIZE = 7;
         int NUMBER_OF_GAMES = 1000;
-        IntStream.range(0, NUMBER_OF_GAMES)
-                .forEach(i -> {
+        List<String> winners = Arrays.stream(new Integer[NUMBER_OF_GAMES])
+                .map(i -> {
                     State initialState = initialState(PLAYER_TYPES, INITIAL_HAND_SIZE);
                     State finalState = playGame(initialState);
-                    System.out.println(finalState.winner().getClass().getSimpleName());
-                });
+                    return finalState.winner().getClass().getSimpleName();
+                })
+                .collect(Collectors.toList());
+        System.out.println("Simple Player Wins: " + winners.stream().filter(winner -> winner.equals("SimplePlayer")).count());
+        System.out.println("Dummy Player Wins: " + winners.stream().filter(winner -> winner.equals("DummyPlayer")).count());
     }
 
     private static State initialState(PlayerTypes[] playerTypes, int initialHandSize) {
