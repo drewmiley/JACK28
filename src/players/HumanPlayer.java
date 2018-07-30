dd6a017334ee77a14b242e728502cdfa2cd17400
Drew Miley <drew.miley@opencastsoftware.com>
2018-07-30 23:08:39 +0100

    Improves human player display

--- src/players/HumanPlayer.java
+++ src/players/HumanPlayer.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-// TODO: Improve display
 class HumanPlayer extends Player {
 
     HumanPlayer(int playerIndex, List<Card> hand) {
@@ -20,10 +19,12 @@ class HumanPlayer extends Player {
     public List<Card> cardsToPlay(Rules rules, Deck deck, Pile pile, List<VisiblePlayer> visiblePlayers) {
         List<List<Card>> possibleCardsToPlay = this.possibleCardsToPlay(rules, pile);
 
-        System.out.println("Select cards to play (0-based index)");
-        possibleCardsToPlay.stream()
-                .map(cardsToPlay -> cardsToPlay.stream().reduce("", (acc, val) -> acc + " " + val.toString(), (a, b) -> a + b))
-                .forEach(System.out::println);
+        System.out.println("Select cards to play");
+        for (int i = 0; i < possibleCardsToPlay.size(); i++) {
+            String cardsToPlayString = possibleCardsToPlay.get(i).stream()
+                    .reduce("", (acc, val) -> acc + " " + val.toString(), (a, b) -> a + b);
+            System.out.println(i + ": " + cardsToPlayString);
+        }
 
         Scanner in = new Scanner(System.in);
         int index = in.nextInt();
@@ -33,10 +34,10 @@ class HumanPlayer extends Player {
 
     @Override
     public Suit nomination(Rules rules, Deck deck, Pile pile, List<VisiblePlayer> visiblePlayers) {
-        System.out.println("Select suit to nominate (0-based index)");
-        Arrays.stream(Suit.values())
-                .map(Enum::toString)
-                .forEach(System.out::println);
+        System.out.println("Select suit to nominate");
+        for (int i = 0; i < Suit.values().length; i++) {
+            System.out.println(i + ": " + Suit.values()[i]);
+        }
 
         Scanner in = new Scanner(System.in);
         int index = in.nextInt();
